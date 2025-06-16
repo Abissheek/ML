890ea872e2461792f221a0d6ac953ee1c5dcb3f3
Abishek <np03cs4s220140@heraldcollege.edu.np>
2025-06-16 14:13:44 +0545

    Model Training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation,DataTransformationConfig
+from src.components.model_train import model_trainer,ModelTrainerConfig
 
 
 @dataclass
@@ -54,4 +55,7 @@ if __name__=="__main__":
 
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer=model_trainer()
+    print(model_trainer.initiate_model_training(train_arr,test_arr))
